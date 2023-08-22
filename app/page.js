f38323d6009b2cbcd6580df30e45f415e02d592c
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-22 17:08:22 -0400

    componente users

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Users from "@/components/Users";
 const fetchUsers = async () => {
   const res = await fetch("https://reqres.in/api/users");
   const data = await res.json();
@@ -9,22 +10,7 @@ const Page = async () => {
   const usersData = users.data;
   return (
     <div>
-      <ul>
-        {usersData.map((user) => (
-          <li
-            key={user.id}
-            className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between "
-          >
-            <div>
-              <h5 className="font-bold">
-                {user.id} {user.first_name} {user.last_name}
-              </h5>
-              <p className="text-slate-100"> email: {user.email}</p>
-            </div>
-            <img src={user.avatar} className="rounded-full w-20" />
-          </li>
-        ))}
-      </ul>
+      <Users usersData={usersData} />
     </div>
   );
 };

7b7de4ee34517dd88f8246aa66a9bf047f52848b
Josemaria Naranjo <josemnaranjoc@gmail.com>
2023-08-22 17:04:20 -0400

    estilo basico

--- app/page.js
+++ app/page.js
@@ -9,16 +9,19 @@ const Page = async () => {
   const usersData = users.data;
   return (
     <div>
-      <h1>Home Page</h1>
       <ul>
         {usersData.map((user) => (
-          <li key={user.id}>
+          <li
+            key={user.id}
+            className="bg-slate-400 mb-2 p-4 rounded-md text-black flex justify-between "
+          >
             <div>
-              <h5>
+              <h5 className="font-bold">
                 {user.id} {user.first_name} {user.last_name}
               </h5>
-              <p> email: {user.email}</p>
+              <p className="text-slate-100"> email: {user.email}</p>
             </div>
+            <img src={user.avatar} className="rounded-full w-20" />
           </li>
         ))}
       </ul>
